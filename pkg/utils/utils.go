e6bd550fa61022eb534dd6b5c1896428d1dbbf0d
zxfishhack <zxfishhack@126.com>
2021-12-01 15:34:14 +0800

    激活终端程序

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -4,14 +4,38 @@ package utils
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 	"os/exec"
+	"path/filepath"
 )
 
+const terminalScript = `
+tell application "Terminal"
+    set newWindow to (do script "ssh %s")
+    tell the application named "Terminal"
+        activate
+    end tell
+end tell
+`
+
+const iTerm2Script = `
+tell application "iTerm2"
+    set newWindow to (create window with default profile command "ssh %s")
+    tell the application named "iTerm2"
+        activate
+    end tell
+end tell
+`
+
 func OpenSSH(name string) (err error) {
-	cmd := exec.Command("osascript", "-e", fmt.Sprintf(`tell application "Terminal" to do script "ssh %s"`, name))
+	fn := filepath.Join(os.TempDir(), "run")
+	script := fmt.Sprintf(iTerm2Script, name)
+	_ = ioutil.WriteFile(fn, []byte(script), 0755)
+	cmd := exec.Command("osascript", fn)
 	err = cmd.Start()
 	if cmd.Process != nil {
-		cmd.Process.Release()
+		_ = cmd.Process.Release()
 	}
 	return
 }
