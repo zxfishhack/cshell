6c807d89f3b45e86a54db58db8d2e58207b369b9
zxfishhack <zxfishhack@126.com>
2021-12-03 11:54:10 +0800

    处理配置文件不存在的问题

--- pkg/sshc/sshc.go
+++ pkg/sshc/sshc.go
@@ -1,6 +1,7 @@
 package sshc
 
 import (
+	"errors"
 	"github.com/zxfishhack/ssh_config"
 	"io/ioutil"
 	"os"
@@ -11,6 +12,12 @@ var sshConfigMutex sync.RWMutex
 
 func LoadSSHConfig(sshFile string) (cfg *ssh_config.Config, err error) {
 	f, err := os.Open(sshFile)
+	// 处理ssh config不存在的问题
+	if errors.Is(err, os.ErrNotExist) {
+		return &ssh_config.Config{
+			Hosts: make([]*ssh_config.Host, 0),
+		}, nil
+	}
 	if err != nil {
 		return
 	}
